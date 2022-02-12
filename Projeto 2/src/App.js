0a2dd5bf9c531de1da3f59bda86f241f8582d0c4
dagwellington <98289604+dagwellington@users.noreply.github.com>
2022-02-11 21:34:11 -0300

    Add files via upload

---
+++ Projeto 2/src/App.js
@@ -0,0 +1,24 @@
+const App = () => {
+    return{
+        <>
+        <h1>Minha primeira aplicação React</h1> 
+        <ul>
+        <Item>
+           Item 1 
+        </Item>            
+        <Item>
+           Item 2 
+        </Item>            
+        <Item>
+           Item 3 
+        </Item>
+            
+        
+        
+        </ul>
+        </>
+    }
+
+}
+
+export default App: 
